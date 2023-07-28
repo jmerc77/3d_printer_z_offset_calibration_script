acc7c6eab0d4fa9814324cddcdd50ab8a83101ef
jmerc77 <24909068+jmerc77@users.noreply.github.com>
2023-07-28 03:51:06 -0400

    Add files via upload

---
+++ Z offset box calibration.py
@@ -0,0 +1,84 @@
+import math
+
+#machine settings
+filament_d=1.75 #mm, always more than 0
+nozzle_d=0.4 #mm, always more than 0
+filament_flow=1 #always more than 0
+
+#filament settings
+retract_fr=30 #mm/sec, always more than 0
+retract=2 #mm, always 0 or more
+linear_adv=-1 #0.06 #negative to disable
+nozzle_temp=215 #C, as reccommended by filament
+bed_temp=60 #C, what works best for filament, 0 to disable
+
+#cali settings
+layer_h=0.2 #mm, always more than 0, half nozzle_d rounded down to nearest step for best results
+z_diff=0.1 #mm, always more than 0, 50 to 75% of layer_h idealy
+
+#bed center (mm)
+cent_x=150
+cent_y=150
+
+#template params
+temp_noz_d=0.4
+temp_w=97.636
+
+def calc_e(l,lh,ew,flow,fil_d):
+    return (lh*flow*ew*l)/(math.pi/4*(fil_d**2))
+
+def scale_translate_gcode(gcode,scale,x,y,x1,y1):
+    if gcode.startswith("G1") and "X" in gcode:
+        x2=float(gcode[gcode.index("X")+1:gcode.index(" ",gcode.index("X"))].replace("-.","-0."))*scale+x
+        y2=float(gcode[gcode.index("Y")+1:gcode.index(" ",gcode.index("Y"))].replace("-.","-0."))*scale+y
+        dist=math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+        return [gcode[:gcode.index("X")+1]+str(round(x2,5))+" Y"+str(round(y2,5))+gcode[gcode.index(" ",gcode.index("Y")):],x2,y2,dist]
+    return [line,x1,y1,-1]
+
+if __name__=="__main__":
+    with open("template.txt") as fr:
+        fw=open("z cali.gcode","w+",newline="\n")
+        lines=fr.readlines()
+        scale=nozzle_d/temp_noz_d
+        cur_x=0
+        cur_y=0
+        cur_e=0
+        temp=""
+        for line in lines:
+            if line.startswith("G1"):
+                move=scale_translate_gcode(line,scale,cent_x,cent_y,cur_x,cur_y)
+                temp_e=0
+                if move[3]>0:
+                    temp_e=calc_e(move[3],layer_h,nozzle_d,filament_flow,filament_d)
+                cur_x=move[1]
+                cur_y=move[2]
+                cur_e+=temp_e
+                temp=move[0]
+                if "[z_cali]" in temp:
+                    temp=temp.replace("[z_cali]",str(round(layer_h+((cur_x-cent_x+temp_w*scale/2)/scale/temp_w*z_diff),5)))
+                if "[z_init]" in temp:
+                    temp=temp.replace("[z_init]",str(round(layer_h,5)))
+                if "[extrude]" in temp:
+                    temp=temp.replace("[extrude]",str(round(cur_e,5)))
+                if "[ret]" in temp:
+                    temp=temp.replace("[ret]",str(round(retract,5)))
+                if "[ret_fr]" in temp:
+                    temp=temp.replace("[ret_fr]",str(round(retract_fr*60,5)))
+            elif line.startswith("G92 E0"):
+                cur_e=0
+                temp=line
+            elif "[lin_adv]" in line:
+                if linear_adv>=0:
+                    temp=line.replace("[lin_adv]",str(round(linear_adv,5)))
+                else:
+                    continue
+            elif "[bed_temp]" in line:
+                temp=line.replace("[bed_temp]",str(round(bed_temp,5)))
+            elif "[noz_temp]" in line:
+                temp=line.replace("[noz_temp]",str(round(nozzle_temp,5)))
+            elif ";keep" in line:
+                temp=line
+            else:
+                continue
+            fw.write(temp)        
+        fw.close()
---
+++ gcode prep.py
@@ -0,0 +1,72 @@
+import math
+# z_cali, z_init, lin_adv, bed_temp, noz_temp, ret_fr, ret, extrude
+if __name__=="__main__":
+    with open("Z offset box.gcode") as fr:
+        fw=open("template.txt","w+",newline="\n")
+        lines=fr.readlines()
+        cali_moves=False
+        temp=""
+        min_x=0
+        max_x=0
+        for line in lines:
+            if line.startswith(";TYPE:Solid infill"):
+                cali_moves=True
+                continue
+            elif line[0]==";":
+                continue
+            if line.startswith("G1"):
+                if cali_moves:
+                    if "Z" in line:
+                        temp=line.replace("Z.2","Z[z_cali]")
+                    elif "E" in line and "X" in line:
+                        temp=line.replace("E","Z[z_cali] E")
+                    else:
+                        temp=line
+                        if temp.startswith("G10"):
+                            temp="G92 E0 ;keep\nG1 E-[ret] F[ret_fr] ;\n"
+                        if temp.startswith("G11"):
+                            temp="G1 E[ret] F[ret_fr] ;\n"
+                        if "X" in temp:
+                            temp=temp[:-1]+" ;\n"
+                    if "z_cali" in temp and "X" in temp:
+                        if "E" in temp:
+                            temp=temp[:-len(temp)+temp.index("E")]+"E[extrude] ;move\n"
+                        else:
+                            temp=temp[:-1]+" ;move\n"
+                    if "X" in temp:
+                        x=float(temp[temp.index("X")+1:temp.index(" ",temp.index("X"))].replace("-.","-0."))
+                        if x<min_x:
+                            min_x=x
+                        if x>max_x:
+                            max_x=x
+                else:
+                    if "Z" in line:
+                        temp=line.replace("Z.2","Z[z_init]")
+                    elif "E" in line and "X" in line:
+                        temp=line.replace("E","Z[z_init] E")
+                    else:
+                        temp=line
+                        if temp.startswith("G10"):
+                            temp="G92 E0 ;keep\nG1 E-[ret] F[ret_fr]\n"
+                        if temp.startswith("G11"):
+                            temp="G1 E[ret] F[ret_fr]\n"
+                        if "X" in temp:
+                            temp=temp[:-1]+" ;\n"
+                    if "z_init" in temp and "X" in temp:
+                        if "E" in temp:
+                            temp=temp[:-len(temp)+temp.index("E")]+"E[extrude] ;move\n"
+                        else:
+                            temp=temp[:-1]+" ;move\n"
+            elif line.startswith("M900"):
+                temp="M900 K[lin_adv]\n"
+            elif line.startswith("M190"):
+                temp="M190 S[bed_temp]\n"
+            elif line.startswith("M109"):
+                temp="M109 S[noz_temp]\n"
+            elif line.startswith("G28") or line.startswith("G29") or line.startswith("G92") or line.startswith("M107") or line.startswith("M82") or line.startswith("G21") or line.startswith("M106") or line.startswith("M140") or line.startswith("M104") or line.startswith("M84") or line.startswith("G29") or line.startswith("G90"):
+                temp=line[:-1]+" ;keep\n"
+            else:
+                continue
+            fw.write(temp)
+        print("temp_w"+str(max_x-min_x))
+    fw.close()
